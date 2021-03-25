1d50650551461ee36d386a907359c18c695aa614
Alexander Eberle <alexander171294@gmail.com>
2021-03-25 16:50:52 -0300

    vac

--- src/app/Api/valve-api.service.ts
+++ src/app/Api/valve-api.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -6,11 +8,27 @@ import { environment } from 'src/environments/environment';
 })
 export class ValveApiService {
 
-  private static readonly steamUserApi = 'https://api.steampowered.com/ISteamUser/GetPlayerBans/v1//?key=' + environment.steamApiKey + '&steamids=${STEAM_ID}&format=json';
+  private static readonly steamUserApi = 'https://api.steampowered.com/ISteamUser/GetPlayerBans/v1?key=' + environment.steamApiKey + '&steamids=${STEAM_ID}&format=json';
 
-  constructor() { }
+  constructor(private httpC: HttpClient) { }
 
-  public getVacs() {
-
+  public getVacs(uid64: string): Observable<PlayerVacsResponse> {
+    return this.httpC.get<PlayerVacsResponse>(
+      ValveApiService.steamUserApi.replace('${STEAM_ID}', uid64)
+    );
   }
+}
+
+export class PlayerVacsResponse {
+  players: VacResponse[];
+}
+
+export class VacResponse {
+  public SteamId: string;
+  public CommunityBanned: boolean;
+  public VACBanned: boolean;
+  public NumberOfVACBans: number;
+  public DaysSinceLastBan: number;
+  public NumberOfGameBans: number;
+  public EconomyBan: string;
 }
