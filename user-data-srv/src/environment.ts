38df44272655be6093c6999108ecb4df9e7e0cf3
Alexander Eberle <alexander171294@gmail.com>
2022-02-13 12:45:37 -0300

    adding readme

--- user-data-srv/src/environment.ts
+++ user-data-srv/src/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   steamApiKey: process.env.STEAM_API ? process.env.STEAM_API : 'FBE6A5C249453A8516A55AAD5F87973F', // https://steamcommunity.com/dev/apikey
   ipstackApiKey: '', // https://ipstack.com/signup/free
   redis: {
-    host: process.env.CACHE_HOST ? process.env.CACHE_HOST : 'srv-captain--redistest',
+    host: process.env.CACHE_HOST ? process.env.CACHE_HOST : 'localhost',
     auth_pass: process.env.CACHE_AUTH ? process.env.CACHE_AUTH : '38674516',
     port: process.env.CACHE_PORT ? parseInt(process.env.CACHE_PORT) : 6379
   },
