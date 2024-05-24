71d72f7ef90ef5a249523b0f313d7d005d8398f4
Alexander Eberle <alexander171294@gmail.com>
2024-05-24 16:29:19 -0300

    adding redis URI

--- user-data-srv/src/environment.ts
+++ user-data-srv/src/environment.ts
@@ -4,6 +4,7 @@ export const environment = {
   steamApiKey: process.env.STEAM_API ? process.env.STEAM_API : 'FBE6A5C249453A8516A55AAD5F87973F', // https://steamcommunity.com/dev/apikey
   ipstackApiKey: '', // https://ipstack.com/signup/free
   redis: {
+    url: process.env.CACHE_URL ? process.env.CACHE_URL : undefined,
     host: process.env.CACHE_HOST ? process.env.CACHE_HOST : 'localhost',
     auth_pass: process.env.CACHE_AUTH ? process.env.CACHE_AUTH : '38674516',
     port: process.env.CACHE_PORT ? parseInt(process.env.CACHE_PORT) : 6379
