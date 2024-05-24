5c2674d6e0e7e41581875a9287c64b3b1992e58b
Alexander Eberle <alexander171294@gmail.com>
2024-05-24 16:55:04 -0300

    fixing toast

--- rustmon/src/app/login/login.module.ts
+++ rustmon/src/app/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ToastModule,
     RouterModule.forChild(routes)
   ]
 })
