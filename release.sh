7158ca5235fcdce4c4c91656fdc03a4f75ce72d7
Alexander Eberle <alexander171294@gmail.com>
2024-05-24 16:04:22 -0300

    fixing docker image to amd/64

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
 echo Version number:
 read version
 
-docker build -t alexander171294/rustmon ./rustmon
+docker build --platform linux/amd64 -t alexander171294/rustmon ./rustmon
 docker tag alexander171294/rustmon:latest alexander171294/rustmon:$version
 docker push alexander171294/rustmon
 docker push alexander171294/rustmon:$version
 
-docker build -t alexander171294/rustmon-service ./user-data-srv
+docker build --platform linux/amd64 -t alexander171294/rustmon-service ./user-data-srv
 docker tag alexander171294/rustmon-service:latest alexander171294/rustmon-service:$version
 docker push alexander171294/rustmon-service
 docker push alexander171294/rustmon-service:$version
